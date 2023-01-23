bec07c7fd075d9f5fe894e651ae092a80f514e32
Training <training@ip-172-31-22-99.ec2.internal>
2023-01-23 22:36:06 +0000

    created two models

---
+++ accounts/models/customer.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from accounts.models.address import Address
+
+class Customer(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    address: Address
+    email: str
+    
+    
